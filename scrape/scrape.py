c89a3fc6cbdbfb309964d9621d5afcb7741b0b34
Alex Cote <apc51104@gmail.com>
2023-11-07 17:00:10 -0500

    Update scrape.py

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -2,35 +2,30 @@ import requests
 from bs4 import BeautifulSoup
 
 # URL of the website you want to scrape
-url = "https://www.statnews.com/2023/08/09/happi-sabeti-sentinel-geneticists-global-pandemic-africa/"  # Replace with the actual URL
+url = 'https://e360.yale.edu/features/delaware-river-clean-water-act'
 
+# Send an HTTP GET request to the URL
+response = requests.get(url)
 
-try:
-    # Send an HTTP GET request to the URL
-    response = requests.get(url)
+# Check if the request was successful
+if response.status_code == 200:
+    # Parse the HTML content of the page using BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the HTML content of the page using BeautifulSoup
-        soup = BeautifulSoup(response.text, 'html.parser')
+    # Find the section with class "article__body"
+    article_body = soup.find('section', class_='article__body')
 
-        # Find the <section> element with the class "the-content"
-        section_element = soup.find('section', class_='the-content')
+    if article_body:
+        # Find all <div> tags within the article__body section
+        div_tags = article_body.find_all('div')
 
-        if section_element:
-            # Find all <p> elements within the <section> element
-            p_elements = section_element.find_all('p')
-
-            # Extract and print the text content of each <p> element
-            for p in p_elements:
-                print(p.get_text())
-        else:
-            print("No <section> element with the class 'the-content' found on the page.")
-
+        # Loop through the div tags and extract the text from paragraph tags
+        for div in div_tags:
+            paragraph = div.find('p')
+            if paragraph:
+                # Print or store the text from the paragraph
+                print(paragraph.get_text())
     else:
-        print(f"Failed to retrieve the web page. Status code: {response.status_code}")
-
-except requests.exceptions.RequestException as e:
-    print(f"An error occurred: {e}")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
+        print("Could not find the article__body section on the page.")
+else:
+    print("Failed to retrieve the webpage. Status code:", response.status_code)
