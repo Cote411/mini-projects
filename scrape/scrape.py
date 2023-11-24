618fe766f6b50b2bc3ebe02e59bfbeb695f7f635
Alex Cote <apc51104@gmail.com>
2023-11-24 15:25:26 -0500

    Update scrape.py

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # URL of the website you want to scrape
-url = "https://e360.yale.edu/features/assisted-migration-endangered-species-climate-change-united-states"
+url = "https://submitty.cs.rpi.edu/courses/f23/csci2500/polls/42"
 
 # Send an HTTP GET request to the URL
 response = requests.get(url)
@@ -13,11 +13,11 @@ if response.status_code == 200:
     soup = BeautifulSoup(response.text, "html.parser")
 
     # Find the section with class "article__body"
-    article_body = soup.find("section", class_="article__body")
+    article_body = soup.find("div", class_="submitty-body")
 
     if article_body:
         # Find all <div> tags within the article__body section
-        div_tags = article_body.find_all("div")
+        div_tags = article_body.find_all("div", class_ = "markdoww")
 
         # Loop through the div tags and extract the text from paragraph tags
         for div in div_tags:
